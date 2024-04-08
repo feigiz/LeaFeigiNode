5f4971a904d000b799e6d3c8c266b256d22bed84
feigi <pz0933@gmail.com>
2024-04-08 18:59:59 +0300

    ח

--- service/queryPost.js
+++ service/queryPost.js
@@ -3,22 +3,17 @@ function getPostQuery(queryparams) {
     const fields = Object.keys(queryparams).filter(param => {
         return param == 'id' || param == 'title' || param == 'body' || param == 'userId';
     });
-    console.log(fields)
     let conditions = "WHERE "
     fields.forEach(field => conditions += field + " = '" + queryparams[field] + "' AND ")
-    console.log(conditions)
-    const query = `SELECT * FROM posts ${fields.length > 0 ? conditions.substring(0, conditions.length - 5) : ""} ${queryparams._sort ? "ORDER BY " + queryparams._sort : ""} ${queryparams._limit ? "LIMIT " + queryparams._limit : ""}`
+    const query = `SELECT * FROM posts ${fields.length > 0 ? conditions.substring(0, conditions.length - 5) : ""} 
+    ${queryparams._sort ? "ORDER BY " + queryparams._sort : ""} 
+    ${queryparams._limit ? "LIMIT " + queryparams._limit : ""}`
     console.log(query)
     return query
 }
 
 function getPostByIdQuery() {
     const query = `SELECT * FROM posts WHERE id = ?`;
-    return query
-}
-
-function getPostByUserIdQuery() {
-    const query = `SELECT * FROM posts WHERE userId = ?`;
     return query
 }
 
@@ -38,5 +33,5 @@ function updatePostQuery() {
 }
 
 export {
-    getPostQuery, getPostByIdQuery, addPostQuery, deletePostQuery, updatePostQuery, getPostByUserIdQuery
+    getPostQuery, getPostByIdQuery, addPostQuery, deletePostQuery, updatePostQuery
 }

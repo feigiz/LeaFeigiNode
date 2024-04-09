08b709f5e2f0780f9db3d18ff24adc884d58e01a
feigi <pz0933@gmail.com>
2024-04-09 17:15:40 +0300

    מחיקת ירוקים בcontroller

--- controllers/postController.js
+++ controllers/postController.js
@@ -4,9 +4,6 @@ export class PostController {
     async getPost(req, res, next) {
         try {
             const postService = new PostService();
-            // const resultItem = await postService.getPost(req.query);
-            // if (resultItem.length > 0)
-            //     return res.status(200).json(resultItem);
             const resultItems = await postService.getPost(req.query);
             return res.status(200).json(resultItems);
         }
@@ -23,12 +20,6 @@ export class PostController {
             const postService = new PostService();
             const resultItem = await postService.getPostById(req.params.id);
             res.status(200).json(resultItem);
-            // if (resultItem)
-            //     res.status(200).json(resultItem);
-            // const err = {}
-            // err.statusCode = 404;
-            // err.message = 'todo not found';
-            // next(err)
         }
         catch (ex) {
             const err = {}
@@ -47,7 +38,6 @@ export class PostController {
         catch (ex) {
             const err = {}
             err.statusCode = 500;
-            // err.statusCode = ex.errno == 1456 ? 400 : 500;
             err.message = ex;
             next(err)
         }
@@ -58,13 +48,6 @@ export class PostController {
             const postService = new PostService();
             await postService.deletePost(req.params.id);
             res.status(200).json(req.params.id);
-            // const resultItem = await postService.deletePost(req.params.id);
-            // if (resultItem.affectedRows > 0)
-            //     res.status(200).json({ status: 200, data: req.params.id });
-            // const err = {}
-            // err.statusCode = 404;
-            // err.message = "post not found";
-            // next(err)
         }
         catch (ex) {
             const err = {}
@@ -78,18 +61,11 @@ export class PostController {
         try {
             const postService = new PostService();
             await postService.updatePost(req.body, req.params.id);
-            // const resultItem = await postService.updatePost(req.body, req.params.id);
-            // if (resultItem.affectedRows > 0)
             res.status(200).json(req.params.id);
-            // const err = {}
-            // err.statusCode = 404;
-            // err.message = "post not found";
-            // next(err)
         }
         catch (ex) {
             const err = {}
             err.statusCode = 500;
-            // err.statusCode = ex.errno == 1054 ? 400 : 500;
             err.message = ex;
             next(err)
         }

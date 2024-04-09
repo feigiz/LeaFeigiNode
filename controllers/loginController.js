b77c8ee011e568f4593b72041726a4bc9b30f4a6
feigiz <pz0933@gmail.com>
2024-04-09 13:58:13 +0300

    user login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,34 @@
+// import { LoginService } from '../service/loginService.js'
+
+// export class LoginController {
+//     async checkUserPassword(req, res, next) {
+//         try {
+//             const loginService = new LoginService();
+//             const resultItem = await loginService.checkUserPassword(req.body.username, req.body.password);
+//             if(resultItem.length==0)
+//             throw new Error('Not found');
+//             res.status(200).json(resultItem);
+//             // res.status(200).json({ status: 200, data: resultItem });
+//         }
+//         catch (ex) {
+//             const err = {}
+//             switch (ex.message) {
+//                 case "Not found":
+//                     err.statusCode = 404;
+//                     break;
+//                 case "Element already exists":
+//                     err.statusCode = 409;
+//                     break;
+//                 default:
+//                     err.statusCode = 500;
+//                     break;
+//             }
+//             err.message = ex.message;
+//             next(err)
+//         }
+//     }
+// }
+
 import { LoginService } from '../service/loginService.js'
 
 export class LoginController {
@@ -5,25 +36,13 @@ export class LoginController {
         try {
             const loginService = new LoginService();
             const resultItem = await loginService.checkUserPassword(req.body.username, req.body.password);
-            if(resultItem.length==0)
-            throw new Error('Not found');
             res.status(200).json(resultItem);
             // res.status(200).json({ status: 200, data: resultItem });
         }
         catch (ex) {
             const err = {}
-            switch (ex.message) {
-                case "Not found":
-                    err.statusCode = 404;
-                    break;
-                case "Element already exists":
-                    err.statusCode = 409;
-                    break;
-                default:
-                    err.statusCode = 500;
-                    break;
-            }
-            err.message = ex.message;
+            err.statusCode = 500;
+            err.message = ex;
             next(err)
         }
     }
--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,8 @@ export class UserController {
             const userService = new UserService();
             console.log(req.query)
             const resultItems = await userService.getUser(req.query);
-            if (resultItems.length > 0)
-                return res.status(200).json(resultItems);
+            // if (resultItems.length > 0)
+            return res.status(200).json(resultItems);
         }
         catch (ex) {
             const err = {}
@@ -21,12 +21,12 @@ export class UserController {
         try {
             const userService = new UserService();
             const resultItem = await userService.getUserById(req.params.id);
-            if (resultItem)
-                res.status(200).json(resultItem);
-            const err = {}
-            err.statusCode = 404;
-            err.message = 'todo not found';
-            next(err)
+            // if (resultItem)
+            res.status(200).json(resultItem);
+            // const err = {}
+            // err.statusCode = 404;
+            // err.message = 'todo not found';
+            // next(err)
         }
         catch (ex) {
             const err = {}
@@ -44,7 +44,8 @@ export class UserController {
         }
         catch (ex) {
             const err = {}
-            err.statusCode = ex.errno == 1062 ? 409 : 500;
+            // err.statusCode = ex.errno == 1062 ? 409 : 500;
+            err.statusCode = 500;
             err.message = ex;
             next(err)
         }
@@ -55,9 +56,8 @@ export class UserController {
         try {
             const userService = new UserService();
             await userService.deleteUser(req.params.id);
-            if (resultItems.affectedRows > 0)
-                res.status(200).json(req.params.id);//מה??
-            // res.status(200).json({ status: 200, data: req.params.id });
+            // if (resultItems.affectedRows > 0)
+            res.status(200).json(req.params.id);
         }
         catch (ex) {
             const err = {}
@@ -71,16 +71,18 @@ export class UserController {
         try {
             const userService = new UserService();
             await userService.updateUser(req.body, req.params.id);
-            if (resultItems.affectedRows > 0)
-                res.status(200).json({ status: 200, data: req.params.id });
-            const err = {}
-            err.statusCode = 404;
-            err.message = "user not found";
-            next(err)
+            res.status(200).json(req.params.id);
+            // if (resultItems.affectedRows > 0)
+            //     res.status(200).json({ status: 200, data: req.params.id });
+            // const err = {}
+            // err.statusCode = 404;
+            // err.message = "user not found";
+            // next(err)
         }
         catch (ex) {
             const err = {}
-            err.statusCode = ex.errno == 1054 ? 400 : 500;
+            err.statusCode = 500;
+            // err.statusCode = ex.errno == 1054 ? 400 : 500;
             err.message = ex;
             next(err)
         }
@@ -90,12 +92,12 @@ export class UserController {
         try {
             const userService = new UserService();
             const resultItem = await userService.updatePassword(req.body);
-            if (resultPassword.affectedRows == 1)
-                res.status(200).json(resultItem.changedRows);
-            const err = {}
-            err.statusCode = 404;
-            err.message = "user not found";
-            next(err)
+            // if (resultPassword.affectedRows == 1)
+            res.status(200).json(resultItem.changedRows);
+            // const err = {}
+            // err.statusCode = 404;
+            // err.message = "user not found";
+            // next(err)
         }
         catch (ex) {
             const err = {}
